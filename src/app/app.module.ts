2756d5cca7b512901df6f3fe5a9091a89dcb75d0
Harini-32 <60243183+Harini-32@users.noreply.github.com>
2020-06-26 23:30:27 -0400

    ICP6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SearchRecipeComponent } from './search-recipe/search-recipe.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchRecipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
